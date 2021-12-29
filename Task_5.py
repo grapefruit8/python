c0480d3c34eb48845039619004a8bc99d1cb0ed0
grapefruit8 <95303568+grapefruit8@users.noreply.github.com>
2021-12-29 11:40:22 +0300

    Add files via upload

---
+++ Task_5.py
@@ -0,0 +1,31 @@
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print(f"Запуск отрисовки {self.title}")
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки ручкой {self.title}")
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки карандашем {self.title}")
+
+
+class Handle(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки маркером {self.title}")
+
+
+pen = Pen('А')
+pen.draw()
+
+pencil = Pencil('Б')
+pencil.draw()
+
+handle = Handle('В')
+handle.draw()
